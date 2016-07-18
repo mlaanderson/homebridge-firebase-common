bf8129301a5695e260d0b125fb9a0da79cf36dcf
Michael Anderson <manderson@amesbury.com>
2016-07-18 16:16:26 -0500

    Working device creation object, including the ability to update Firebase

--- homebridge-firebase-common.bundle.js
+++ homebridge-firebase-common.bundle.js
@@ -1023,18 +1023,148 @@ function hasOwnProperty(obj, prop) {
 }).call(this,require('_process'),typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./support/isBuffer":4,"_process":3,"inherits":2}],6:[function(require,module,exports){
 // homebridge-firebase-common
+"use strict";
 var Platform = require('./lib/platform.js');
 var Service = require('./lib/service.js');
 var Accessory = require('./lib/accessory.js');
-var HapTypes = require('hap-nodejs-types');
+var Types = require('hap-nodejs-types');
+var util = require('util');
+
+function addProp(self, o, sn, cn) {
+    Object.defineProperty(o, cn, {
+        get: function() {
+            return self._data.Services[sn].Characteristics[cn];
+        },
+        set: function(val) {
+            self._data.Services[sn].Characteristics[cn] = val;
+        }
+    });
+}
+
+function DeviceDescriptor(deviceRoot) {
+   
+    this.addService = function(serviceName, optionalCharacteristics, defaultData) {
+        if (false == serviceName in Types.Service) {
+            throw "ERROR: Invalid Service Name (" + serviceName + ")";
+        }
+        
+        var service = Types.Service[serviceName];
+        var characteristics = service.Characteristics;
+        var result = { Characteristics: {} };
+        
+        if ((optionalCharacteristics === undefined) || (optionalCharacteristics == null)) {
+            optionalCharacteristics = [];
+        } else if (util.isBoolean(optionalCharacteristics) == true) {
+            if (optionalCharacteristics) {
+                optionalCharacteristics = service.OptionalCharacteristics;
+            } else {
+                optionalCharacteristics = [];
+            }
+        } else if (util.isArray(optionalCharacteristics) == false) {
+            if ((util.isObject(optionalCharacteristics) == true) && (typeof defaultData == "undefined")) {
+                defaultData = optionalCharacteristics;
+                optionalCharacteristics = [];
+            } else {
+                throw "ERROR: Invalid Argument, optionalCharacteristics must be boolean or array of strings";
+            }
+        }
+        
+        defaultData = defaultData || {};
+        
+        characteristics = characteristics.concat(optionalCharacteristics);
+        
+        
+        for (var n = 0; n < characteristics.length; n++) {
+            var characteristicName = characteristics[n];
+            var characteristic = Types.Characteristic[characteristicName];
+
+            if (true == characteristicName in defaultData) {
+                result.Characteristics[characteristicName] = defaultData[characteristicName];
+            } else {
+                switch (characteristic.format) {
+                    case Types.Characteristic.Formats.BOOL:
+                        result.Characteristics[characteristicName] = false;
+                        break;
+                    case Types.Characteristic.Formats.INT:
+                    case Types.Characteristic.Formats.FLOAT:
+                    case Types.Characteristic.Formats.UINT8:
+                    case Types.Characteristic.Formats.UINT16:
+                    case Types.Characteristic.Formats.UINT32:
+                    case Types.Characteristic.Formats.UINT64:
+                    case Types.Characteristic.Formats.TLV8:
+                        result.Characteristics[characteristicName] = 0;
+                        break;
+                    case Types.Characteristic.Formats.DATA:
+                    case Types.Characteristic.Formats.STRING:
+                        result.Characteristics[characteristicName] = "";
+                        break;
+                    case Types.Characteristic.Formats.ARRAY:
+                        result.Characteristics[characteristicName] = [];
+                        break;
+                    case Types.Characteristic.Formats.DICTIONARY:
+                        result.Characteristics[characteristicName] = {};
+                        break;
+                }
+            }
+        }
+        
+        this._data.Services[serviceName] = result;
+    }
+    
+    this.removeService = function(serviceName) {
+        delete this._data.Services[serviceName];
+    }
+    
+    this.pushToFirebase = function(ref) {
+        ref.child(deviceRoot).once('value', (function(snapshot) {
+            var data = snapshot.val();
+            var val = this._data;
+            
+            if ((data !== null) && ('Services' in data)) {
+                val = this._data.Services;
+                snapshot.ref().child('Services').update(val);
+            } else {
+                snapshot.ref().update(this._data);
+            }
+        }).bind(this));
+    }
+    
+    Object.defineProperty(this, 'Services', {
+        get: function() {
+            var obj = {};
+            
+            for (var sn in this._data.Services) {
+                var sobj = {};
+                Object.defineProperty(obj, sn, { value: sobj });
+                
+                for (var cn in this._data.Services[sn].Characteristics) {
+                    addProp(this, sobj, sn, cn);
+                }
+            }
+            
+            return obj;
+        }
+    });
+    
+    this.toJSON = function() {
+        return this._data;
+    }
+
+    this._root = deviceRoot;
+    this._data = {
+        "Services": {}
+    }
+}
+
 
 module.exports = {
     Platform: Platform,
     Accessory: Accessory,
     Service: Service,
-    Types: HapTypes
+    Types: Types,
+    Device: DeviceDescriptor
 }
-},{"./lib/accessory.js":7,"./lib/platform.js":8,"./lib/service.js":9,"hap-nodejs-types":11}],7:[function(require,module,exports){
+},{"./lib/accessory.js":7,"./lib/platform.js":8,"./lib/service.js":9,"hap-nodejs-types":11,"util":5}],7:[function(require,module,exports){
 var EventEmitter = require('events');
 var util = require('util');
 var Service = require('./service.js');
@@ -1058,16 +1188,31 @@ function Accessory(ref) {
     this._serviceNames = [];
     this._services = [];
     
-    function _nameHandler(snapshot) {
-        _shadow.Name = snapshot.val();
-        this.emit('Name', _shadow.Name);
-    }
-    
     function _checkReady() {
         if (_ready) return;
         
         for (var n = 0; n < this._services.length; n++) {
             if (this._services[n].IsReady == false) return;
+        }
+        
+        if ((this.AccessoryInformation !== undefined) && (this.AccessoryInformation.Name !== undefined)) {
+            Object.defineProperty(this, 'Name', {
+                get: function() {
+                    return this.AccessoryInformation.Name;
+                },
+                set: function(value) {
+                    this.AccessoryInformation.Name = value;
+                }
+            });
+        } else if (this._services[this.serviceNames[0]].Name !== undefined) {
+            Object.defineProperty(this, 'Name', {
+                get: function() {
+                    return this._services[this.serviceNames[0]].Name;
+                },
+                set: function(value) {
+                    this._services[this.serviceNames[0]].Name = value;
+                }
+            });
         }
         
         _ready = true;
@@ -1076,16 +1221,6 @@ function Accessory(ref) {
     
     function _scanServices(snapshot) {
         var service = snapshot.val();
-        
-        // add the name listener and property
-        Object.defineProperty(this, 'Name', {
-            get: function() { return _shadow.Name; },
-            set: function(val) { 
-                _shadow.Name = val;
-                _ref.child('Name').set(val);
-            }
-        });
-        _ref.child('Name').on('value', _nameHandler.bind(this));
         
         for (var k in service.Services) {
             defineServiceProperty(this, ref, k);
--- index.js
+++ index.js
@@ -1,12 +1,142 @@
 // homebridge-firebase-common
+"use strict";
 var Platform = require('./lib/platform.js');
 var Service = require('./lib/service.js');
 var Accessory = require('./lib/accessory.js');
-var HapTypes = require('hap-nodejs-types');
+var Types = require('hap-nodejs-types');
+var util = require('util');
+
+function addProp(self, o, sn, cn) {
+    Object.defineProperty(o, cn, {
+        get: function() {
+            return self._data.Services[sn].Characteristics[cn];
+        },
+        set: function(val) {
+            self._data.Services[sn].Characteristics[cn] = val;
+        }
+    });
+}
+
+function DeviceDescriptor(deviceRoot) {
+   
+    this.addService = function(serviceName, optionalCharacteristics, defaultData) {
+        if (false == serviceName in Types.Service) {
+            throw "ERROR: Invalid Service Name (" + serviceName + ")";
+        }
+        
+        var service = Types.Service[serviceName];
+        var characteristics = service.Characteristics;
+        var result = { Characteristics: {} };
+        
+        if ((optionalCharacteristics === undefined) || (optionalCharacteristics == null)) {
+            optionalCharacteristics = [];
+        } else if (util.isBoolean(optionalCharacteristics) == true) {
+            if (optionalCharacteristics) {
+                optionalCharacteristics = service.OptionalCharacteristics;
+            } else {
+                optionalCharacteristics = [];
+            }
+        } else if (util.isArray(optionalCharacteristics) == false) {
+            if ((util.isObject(optionalCharacteristics) == true) && (typeof defaultData == "undefined")) {
+                defaultData = optionalCharacteristics;
+                optionalCharacteristics = [];
+            } else {
+                throw "ERROR: Invalid Argument, optionalCharacteristics must be boolean or array of strings";
+            }
+        }
+        
+        defaultData = defaultData || {};
+        
+        characteristics = characteristics.concat(optionalCharacteristics);
+        
+        
+        for (var n = 0; n < characteristics.length; n++) {
+            var characteristicName = characteristics[n];
+            var characteristic = Types.Characteristic[characteristicName];
+
+            if (true == characteristicName in defaultData) {
+                result.Characteristics[characteristicName] = defaultData[characteristicName];
+            } else {
+                switch (characteristic.format) {
+                    case Types.Characteristic.Formats.BOOL:
+                        result.Characteristics[characteristicName] = false;
+                        break;
+                    case Types.Characteristic.Formats.INT:
+                    case Types.Characteristic.Formats.FLOAT:
+                    case Types.Characteristic.Formats.UINT8:
+                    case Types.Characteristic.Formats.UINT16:
+                    case Types.Characteristic.Formats.UINT32:
+                    case Types.Characteristic.Formats.UINT64:
+                    case Types.Characteristic.Formats.TLV8:
+                        result.Characteristics[characteristicName] = 0;
+                        break;
+                    case Types.Characteristic.Formats.DATA:
+                    case Types.Characteristic.Formats.STRING:
+                        result.Characteristics[characteristicName] = "";
+                        break;
+                    case Types.Characteristic.Formats.ARRAY:
+                        result.Characteristics[characteristicName] = [];
+                        break;
+                    case Types.Characteristic.Formats.DICTIONARY:
+                        result.Characteristics[characteristicName] = {};
+                        break;
+                }
+            }
+        }
+        
+        this._data.Services[serviceName] = result;
+    }
+    
+    this.removeService = function(serviceName) {
+        delete this._data.Services[serviceName];
+    }
+    
+    this.pushToFirebase = function(ref) {
+        ref.child(deviceRoot).once('value', (function(snapshot) {
+            var data = snapshot.val();
+            var val = this._data;
+            
+            if ((data !== null) && ('Services' in data)) {
+                val = this._data.Services;
+                snapshot.ref().child('Services').update(val);
+            } else {
+                snapshot.ref().update(this._data);
+            }
+        }).bind(this));
+    }
+    
+    Object.defineProperty(this, 'Services', {
+        get: function() {
+            var obj = {};
+            
+            for (var sn in this._data.Services) {
+                var sobj = {};
+                Object.defineProperty(obj, sn, { value: sobj });
+                
+                for (var cn in this._data.Services[sn].Characteristics) {
+                    addProp(this, sobj, sn, cn);
+                }
+            }
+            
+            return obj;
+        }
+    });
+    
+    this.toJSON = function() {
+        return this._data;
+    }
+
+    this._root = deviceRoot;
+    this._data = {
+        "Services": {}
+    }
+}
+
 
 module.exports = {
     Platform: Platform,
     Accessory: Accessory,
     Service: Service,
-    Types: HapTypes
+    Types: Types,
+    Device: DeviceDescriptor
 }
