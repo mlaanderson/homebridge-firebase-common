3977fed90ced1349e760e8ef407bfcfcdb4ad001
Michael Anderson <manderson@amesbury.com>
2016-07-18 11:26:19 -0500

    First stab at the firebase platform

--- examples/homebridge-accessory/homebridge-accessory.js
+++ examples/homebridge-accessory/homebridge-accessory.js
@@ -11,26 +11,63 @@ module.exports = function(homebridge) {
     homebridge.registerAccessory("homebridge-accessory", "FirebaseAccessory", FirebaseAccessory);
 }
 
+module.exports.FirebaseAccessory = FirebaseAccessory;
+
 class FirebaseAccessory {
     constructor(log, config) {
-        this.log = log;
-        this._config = config;
-        
-        this._db = new Firebase(this._config.firebase_host);
-        this._accessory = new AccessoryBase(this._db.child(this._config.firebase_path));
-        this._accessory.ready(this._accessoryReady.bind(this));
-        
-        this._db.authWithPassword({
-            email: this._config.username,
-            password: this._config.password
-        });
-        
-        this._serviceMap = {};
-        this._characteristicMap = {};
-        
-        for (var serviceName of this._config.services) {
-            this._serviceMap[serviceName] = new Service[serviceName]();
-            this._characteristicMap[serviceName] = {};
+        if (AccessoryBase.prototype.isPrototypeOf(config) == true) {
+            // this Accessory is being constructed as a member of a platform
+            this.log = log;
+            this._accessory = config;
+            this._config = { Name: 'Unknown' };
+            this._ready = false;
+            
+            // create the service and characteristic map structures
+            this._serviceMap = {};
+            this._characteristicMap = {};
+            
+            // Generally the accessory should be ready
+            // and this will not be an event driven callback
+            this._accessory.ready((function() {
+                for (var serviceName of this._accessory.Services) {
+                    this._serviceMap[serviceName] = new Service[serviceName]();
+                    this._characteristicMap[serviceName] = {};
+                }
+                this._accessoryReady();
+                
+                if ((this._accessory.AccessoryInformation !== undefined) &&
+                    (this._accessory.AccessoryInformation.Name !== undefined)
+                ) {
+                    this._config.Name = this._accessory.AccessoryInformation.Name;
+                }
+                
+                this._ready = true;
+            }).bind(this));
+            
+            Object.createProperty(this, "IsReady", { get: function() { return this._ready; } });
+            });
+        } else {
+            // this Accessory is being constructed standalong
+            this.log = log;
+            this._config = config;
+            
+            this._db = new Firebase(this._config.firebase_host);
+            this._accessory = new AccessoryBase(this._db.child(this._config.firebase_path));
+            
+            this._db.authWithPassword({
+                email: this._config.username,
+                password: this._config.password
+            });
+            
+            this._serviceMap = {};
+            this._characteristicMap = {};
+            
+            for (var serviceName of this._config.services) {
+                this._serviceMap[serviceName] = new Service[serviceName]();
+                this._characteristicMap[serviceName] = {};
+            }
+
+            this._accessory.ready(this._accessoryReady.bind(this));
         }
     }
     
--- examples/homebridge-platform/index.js
+++ examples/homebridge-platform/index.js
@@ -1,13 +1,69 @@
-var types = require("HAP-NodeJS-Types").TypeUUIDs;
+"use strict";
+
+var Firebase = require("firebase");
+var PlatformBase = require("homebridge-firebase-common").Platform;
+var AccessoryBase = require("../homebridge-accessory/homebridge-accessory.js").FirebaseAccessory;
+var Types = require("hap-nodejs-types");
+
+var Accessory, Characteristic, Service, uuid;
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    Accessory = homebridge.hap.Accessory;
+    uuid = homebridge.hap.uuid;
+    
+    homebridge.registerPlatform("homebridge-platform", "FirebasePlatform", FirebasePlatform);
+}
 
 class FirebasePlatform {
     constructor(log, config) {
         this._log = log;
         this._config = config;
+        this._accessoryCallback = null;
+        this._accessories = [];
+        
+        this._config.firebase_filter = this._config.firebase_filter || /.*/;
+        this._config.firebase_filter = new RegExp(this._config.firebase_filter);
+        
+        this._db = new Firebase(this._config.firebase_host);
+        this._platform = new PlatformBase(this._db.child(this._config.firebase_path), 
+            this._config.firebase_filter);
+            
+        this._platform.ready(this._mapAccessories.bind(this));
+        this._db.authWithPassword({
+            email: this._config.username,
+            password: this._config.password
+        });
+        
+        this._log('Starting loading accessories');
+    }
+    
+    _mapAccessories() {
+        this._log('Mapping accessories');
+        // we know that all the accessories are ready here
+        for (var accessoryName of this._platform.Accessories) {
+            var accessory = new AccessoryBase(this._log, this._platform[accessoryName]);
+            this._accessories.push(accessory);
+        }
+        
+        if (this._accessoryCallback != null) {
+            this._log('Calling the accessories callback for Homebridge');
+            this._accessoryCallback(this._accessories);
+            this._accessoryCallback = null;
+        }
     }
     
     accessories(callback) {
         // if all the accessories have been found
         // just execute the callback
+        
+        this._log("accessories called by Homebridge");
+        if (this._platform.IsReady == true) {
+            this._log('Running the accessories callback for Homebridge');
+            callback(this._accessories);
+        } else {
+            this._accessoryCallback = callback;
+        }
     }
 }
