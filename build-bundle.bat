4e3614c710a790163a6284f39c86b8f6b063345b
Michael Anderson <manderson@amesbury.com>
2016-08-08 10:25:45 -0500

    Updated build-bundle to allow --no-min, --no-mangle and --debug (both --no-min --no-mangle)

--- build-bundle.bat
+++ build-bundle.bat
@@ -1,5 +1,36 @@
 @echo off
 
+SET MINIFY=1
+SET MANGLE=1
+
+:loop
+IF NOT "%1"=="" (
+    if "%1"=="--no-min" (
+        SET MINIFY=0
+    )
+    if "%1"=="--no-mangle" (
+        SET MANGLE=0
+    )
+    if "%1"=="--debug" (
+        SET MINIFY=0
+        SET MANGLE=0
+    )
+    shift
+    GOTO :loop
+)
+
 echo Creating the bundle
-browserify -s homebridge index.js | uglifyjs --mangle --compress -- - > homebridge-firebase-common.bundle.js
+IF "%MINIFY%%MANGLE%"=="00" (
+    browserify -s homebridge index.js > homebridge-firebase-common.bundle.js
+) ELSE (
+    IF "%MINIFY%"=="0" (
+        browserify -s homebridge index.js | uglifyjs --mangle -- - > homebridge-firebase-common.bundle.js
+    ) ELSE (
+        IF "%MANGLE%"=="0" (
+            browserify -s homebridge index.js | uglifyjs --compress -- - > homebridge-firebase-common.bundle.js
+        ) ELSE (
+            browserify -s homebridge index.js | uglifyjs --mangle --compress -- - > homebridge-firebase-common.bundle.js
+        )
+    )
+)
 echo DONE
--- build-bundle.sh
+++ build-bundle.sh
@@ -1,5 +1,42 @@
-#!/bin/sh
+#!/bin/bash
+
+MINIFY=1
+MANGLE=1
+MINCMD="uglifyjs --mangle --compress -- -"
+
+while [[ $# -ge 1 ]]
+do
+    key="$1"
+
+    case ${key} in
+        --no-min)
+            MINIFY=0
+            ;;
+        --no-mangle)
+            MANGLE=0
+            ;;
+        --debug)
+            MINIFY=0
+            MANGLE=0
+            ;;
+        *)
+            echo Usage: $0 [--no-min][--no-mangle]
+            exit 1
+        ;;
+    esac
+    shift
+done
+
+if [[ ${MINIFY} -eq 1 && ${MANGLE} -eq 1 ]]; then
+    echo "Already set" >> /dev/null
+elif [[ ${MINIFY} -eq 1 ]]; then
+    MINCMD="uglifyjs --compress -- -"
+elif [[ ${MANGLE} -eq 1 ]]; then
+    MINCMD="uglifyjs --mangle -- -"
+else
+    MINCMD="cat -"
+fi
 
 echo Creating the bundle
-browserify -s homebridge index.js | uglifyjs --mangle --compress -- - > homebridge-firebase-common.bundle.js
+browserify -s homebridge index.js | ${MINCMD} > homebridge-firebase-common.bundle.js
 echo DONE
